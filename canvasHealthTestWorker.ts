118187e7ad27710827663f916359919ab2d5a110
magenish <53221799+magenish@users.noreply.github.com>
2023-01-09 11:44:51 +0200

    Update canvasHealthTestWorker.ts

--- canvasHealthTestWorker.ts
+++ canvasHealthTestWorker.ts
@@ -1,24 +1,25 @@
 var globalCanvas = undefined;
-self.onmessage = function(evt) {
-    globalCanvas = evt.data.canvas;
-    createTestableCanvasElement();  
-    // spin up periodical health test.
-    setInterval(healthTest, 1000);
-  }; 
- 
- enum PixelCompareResult {
-    success = 0,
-    failedWithBlackColor = 1,
-    failed = 2,
-  }
-  
-  enum CanvasHealthCheckResult {
-    healthy = 0,
-    failedToRunCheck = 1,
-    notHealthyBuffersEmpty = 2,
-    notHealthyRandomValues = 3,
-  }
+var cnt = 0;
+const font = "50px serif";
+self.onmessage = function (evt) {
+  globalCanvas = evt.data.canvas;
+  createTestableCanvasElement();
+  // spin up periodical health test.
+  setInterval(healthTest, 1000);
+};
 
+enum PixelCompareResult {
+  success = 0,
+  failedWithBlackColor = 1,
+  failed = 2,
+}
+
+enum CanvasHealthCheckResult {
+  healthy = 0,
+  failedToRunCheck = 1,
+  notHealthyBuffersEmpty = 2,
+  notHealthyRandomValues = 3,
+}
 
 function healthTest() {
   var imgData = globalCanvas.getContext("2d").getImageData(0, 0, 2, 2);
@@ -43,15 +44,14 @@ function healthTest() {
 function createTestableCanvasElement() {
   try {
     globalCanvas.id = "myCanvas";
-      // Set the canvas dimensions
-      globalCanvas.width = "400";
-      globalCanvas.height = "400";
-
-      var ctx = globalCanvas.getContext("2d", { willReadFrequently: false });
-      drawSamplePatternOnCanvas(ctx);
-  }
-  catch (exception) {
-      return null;
+    // Set the canvas dimensions
+    globalCanvas.width = "400";
+    globalCanvas.height = "400";
+    cnt++;
+    var ctx = globalCanvas.getContext("2d", { willReadFrequently: false });
+    drawSamplePatternOnCanvas(ctx);
+  } catch (exception) {
+    return null;
   }
 }
 
@@ -59,7 +59,11 @@ function validateCanvasPattern(imageData) {
   const redResult = validatePixel(imageData, { color: "red", x: 0, y: 0 });
   const greenResult = validatePixel(imageData, { color: "green", x: 1, y: 0 });
   const blueResult = validatePixel(imageData, { color: "blue", x: 0, y: 1 });
-  const yellowResult = validatePixel(imageData, { color: "yellow", x: 1, y: 1 });
+  const yellowResult = validatePixel(imageData, {
+    color: "yellow",
+    x: 1,
+    y: 1,
+  });
 
   if (
     redResult === PixelCompareResult.success &&
@@ -91,10 +95,12 @@ function validatePixel(imageData, pixel) {
   const blue = imageData.data[offset + 2];
   const alpha = imageData.data[offset + 3];
 
-  if (!arePixelByteWithinErrorRange(red, 255) ||
-  !arePixelByteWithinErrorRange(green, 0) ||
-  !arePixelByteWithinErrorRange(blue, 0) ||
-  !arePixelByteWithinErrorRange(alpha, 255)) {
+  if (
+    !arePixelByteWithinErrorRange(red, 255) ||
+    !arePixelByteWithinErrorRange(green, 0) ||
+    !arePixelByteWithinErrorRange(blue, 0) ||
+    !arePixelByteWithinErrorRange(alpha, 255)
+  ) {
     if (!red && !green && !blue && !alpha) {
       // pixel buffer is empty (black pixel = 0x000000)
       return PixelCompareResult.failedWithBlackColor;
@@ -125,12 +131,14 @@ function arePixelByteWithinErrorRange(byte1, byte2) {
  * |  Blue  | Yellow |
  */
 function drawSamplePatternOnCanvas(ctx) {
-    drawPixel(ctx, { color: "red", x: 0, y: 0 });
-    drawPixel(ctx, { color: "green", x: 100, y: 0 });
-    drawPixel(ctx, { color: "blue", x: 0, y: 100 });
-    drawPixel(ctx, { color: "yellow", x: 100, y: 100 });
+  drawPixel(ctx, { color: "red", x: 0, y: 0 });
+  drawPixel(ctx, { color: "green", x: 100, y: 0 });
+  drawPixel(ctx, { color: "blue", x: 0, y: 100 });
+  drawPixel(ctx, { color: "yellow", x: 100, y: 100 });
 }
 function drawPixel(ctx, pixel) {
-    ctx.fillStyle = pixel.color;
-    ctx.fillRect(pixel.x, pixel.y, 100, 100);
+  ctx.fillStyle = pixel.color;
+  ctx.fillRect(pixel.x, pixel.y, 100, 100);
+  ctx.font = font;
+  ctx.fillText(cnt.toString(), pixel.x, pixel.y);
 }
