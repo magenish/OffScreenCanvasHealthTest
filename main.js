c87dbca0f8bd2b6a95fca05f991c6282ed39a985
magenish <53221799+magenish@users.noreply.github.com>
2023-01-10 17:12:50 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-var worker = new Worker("canvasHealthTestWorker.js");
-var offscreen = document
+var canvas = document.getElementById("myCanvas");
+canvas.style.position = "absolute";
+canvas.style.zIndex = "9999";
+const worker = new Worker("canvasHealthTestWorker.js");
+const offscreen = document
     .querySelector("canvas")
     .transferControlToOffscreen();
 worker.postMessage({ canvas: offscreen }, [offscreen]);
