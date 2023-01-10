aaed4f5609c23227dfd36fe55bb8f8e0f446ce57
magenish <53221799+magenish@users.noreply.github.com>
2023-01-10 17:12:32 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
+var canvas = document.getElementById("myCanvas");
+canvas.style.position = "absolute";
+canvas.style.zIndex = "9999";
+
 const worker = new Worker("canvasHealthTestWorker.js");
 const offscreen = document
 .querySelector("canvas")
